dc8e2ba7b155e19962d396c98c18589e7f3f8f7f
bigwangsmash <bigwangsmash@lawn-128-61-48-236.lawn.gatech.edu>
2015-03-23 22:27:46 -0400

    LOL

--- MatrixAlgebra.java
+++ MatrixAlgebra.java
@@ -11,12 +11,23 @@ import java.util.Vector;
 public class MatrixAlgebra {
 
     public static Matrix transpose(Matrix m) {
-
+        double[][] holder = new double[m.height][m.width];
+        for (int row = 0; row < m.height; row++) {
+            for (int col = 0; col < m.width; col++) {
+                holder[col][row] = m.get(row, col)
+            }
+        }
+        return new Matrix(holder);
     }
 
+<<<<<<< Updated upstream
 
     public static Matrix dotProductMultiply(Matrix m1, Matrix m2)
         throws IllegalArgumentException {
+=======
+    public static Matrix matrixMultiply(Matrix m1, Matrix m2)
+        throws IllegalOperandException {
+>>>>>>> Stashed changes
         if (m1.width() != m2.height) {
             throw new IllegalArgumentException("Cannot multiply a matrix"
                                                 + " of width " + m1.width
@@ -25,14 +36,11 @@ public class MatrixAlgebra {
         }
 
         double[][] holder = new double[m1.height][m2.width];
-        // for (int holdIndex = 0; holdIndex < holder.length; holdIndex++) {
-        //     double sumOfProducts = 0;
-        //     for (int index = 0; index < v.getLength(); index++) {
-        //         sumOfProducts = sumOfProducts
-        //                         + (m.get(holdIndex, index) * v.get(index));
-        //     }
-        //     holder[holdIndex] = sumOfProducts;
-        // }
+        for (int i = 0; i < m1.height; i++) {
+            for (int j = 0; j < m2.width; j++) {
+
+            }
+        }
         return new Matrix(holder);
     }
 
@@ -61,4 +69,6 @@ public class MatrixAlgebra {
         }
         return new Matrix(holder);
     }
+
+    publ
 }
